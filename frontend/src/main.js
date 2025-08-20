f88263554aa34f8c54cf12572928672148684425
Noah-zipit <noahext994@gmail.com>
2025-08-20 20:52:01 +0100

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
- import App from './App.svelte';
+import App from './App.svelte';
 import './styles/global.css';
+import './styles/mobile-fix.css'; // Add this line
 
 const app = new App({
   target: document.getElementById('app'),
